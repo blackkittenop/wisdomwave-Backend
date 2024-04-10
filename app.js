17acfddfd24a8137f892ce75b0a96cdd7bc37c2a
blackkittenop <hardikthakre17@gmail.com>
2024-04-10 05:44:29 +0530

    working 2

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({
 }));
 app.use(cookieParser());
 app.use(cors({
-    origin:process.env.FRONTEND_URL,
+    origin:"http://localhost:3000",
     credentials:true,
     methods:["GET","POST","PUT","DELETE"],
 }));
