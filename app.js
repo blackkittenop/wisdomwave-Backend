ef49cccf880ce4324d94c824a856fda9aacdd309
blackkittenop <hardikthakre17@gmail.com>
2024-04-15 09:14:55 +0530

    make changes in cors

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({
 }));
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:process.env.FRONTEND_URL,
     credentials:true,
     methods:["GET","POST","PUT","DELETE"],
 }));
